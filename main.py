d6ffa6230593201fcbc57bc568b4e51b04771240
Agathabsantos <agathabsantos2@gmail.com>
2025-08-31 23:38:48 -0300

    Inicio scrapy do site indeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scrapers import vagas_scraper
 from scrapers import programathor_scraper
 from scrapers import empregos_scraper
+from scrapers import indeed_scraper
 from database.db import create_table
 
 def scrape_all():
@@ -13,6 +14,7 @@ def scrape_all():
     total_saved += vagas_scraper.scrape_vagas("desenvolvedor", pages=2)
     total_saved += programathor_scraper.scrape_programathor(num_paginas=2)
     total_saved += empregos_scraper.scrape_empregos2("desenvolvedor", num_paginas=2)
+    total_saved += indeed_scraper.scrape_indeed("desenvolvedor", num_paginas=2)
     
     # Se futuramente adicionar outros scrapers:
     # from scrapers import indeed_scraper
